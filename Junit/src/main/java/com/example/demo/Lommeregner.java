a4ffbb4d2c62be959c12efade77b666c640c8d95
Alex8943 <65241585+Alex8943@users.noreply.github.com>
2021-02-24 15:46:37 +0100

    added a primeNumber arrayList

--- Junit/src/main/java/com/example/demo/Lommeregner.java
+++ Junit/src/main/java/com/example/demo/Lommeregner.java
@@ -6,9 +6,10 @@ public class Lommeregner {
 
     public static void main(String[] args) {
         Calculator cal = new Calculator();
-        System.out.println(cal.add(17, 25)); // 42
-        System.out.println(cal.add(2, 2)); // 4
+        //System.out.println(cal.add(17, 25)); // 42
+        //System.out.println(cal.add(2, 2)); // 4
+        System.out.println(cal.isPrime(10));
 
-        System.out.println(cal.isPrime(31));
+        System.out.println(cal.primeFactorization(10));
     }
 }
--- Junit/src/main/java/com/example/demo/service/Calculator.java
+++ Junit/src/main/java/com/example/demo/service/Calculator.java
@@ -1,22 +1,60 @@
 package com.example.demo.service;
 
+import java.util.ArrayList;
+
 public class Calculator {
 
     //Add 2 numbers
-    public int add(int x, int y){
-        return x+y;
+    public int add(int x, int y) {
+        return x + y;
     }
 
-    public boolean isPrime(int num){
+
+    public ArrayList<Integer> primeFactorization(int num) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int n = 2; n < num; n++) {
+            boolean status = true;
+            int i = 2;
+            while (i <= n / 2) {
+                if (n % i == 0) {
+                    status = false;
+                    break;
+                }
+                i++;
+            }
+            if (status) {
+                arrayList.add(n);
+            }
+        }
+        return arrayList;
+    }
+
+    public boolean isPrime(int num) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        boolean status = true;
+        int i = 2;
+        while (i <= num / 2) {
+            if (num % i == 0) {
+                status = false;
+                break;
+            }
+            i++;
+        }
+        if (status) {
+            arrayList.add(num);
+        }
+        return status;
+    }
+}/*
+        return arrayList;
         boolean status = false;
         for (int i = 2; i <= num; i++) {
             if (num%i == 0 && num > i){
                 return status;
             } else if (i == num){
-
                 return status = true;
             }
         }
         return status;
     }
-}
+    */
